695feadff5b708d3cc4c94c88e30ece2d07274f8
ReplayCoding <replaycoding@gmail.com>
2024-08-08 17:46:51 -0400

    write handler for A2S_GETCHALLENGE

--- src/connectionless.rs
+++ src/connectionless.rs
@@ -1,5 +1,65 @@
 //! Handles the connectionless part of the source engine network protocol
+use std::{
+    hash::{DefaultHasher, Hash, Hasher},
+    io::Cursor,
+    net::SocketAddr,
+};
 
-pub fn process_connectionless_packet(_packet_buf: &[u8]) {
-    todo!("process connectionless packet");
+use anyhow::anyhow;
+use bitstream_io::{BitRead, BitReader, BitWrite, BitWriter, LittleEndian};
+use log::trace;
+
+use crate::{PacketInfo, CONNECTIONLESS_HEADER};
+
+const PROTOCOL_HASHEDCDKEY: u32 = 2;
+
+/// Create an opaque challenge number for an address, which will be consistent for this address
+fn create_challenge_for_address(addr: SocketAddr) -> u32 {
+    let mut hasher = DefaultHasher::new();
+    addr.hash(&mut hasher);
+
+    hasher.finish() as u32
+}
+
+fn handle_get_challenge<R: BitRead>(packet: &PacketInfo, reader: &mut R) -> anyhow::Result<()> {
+    let client_challenge: u32 = reader.read(32)?;
+    let server_challenge = create_challenge_for_address(packet.from);
+
+    trace!("got challenge {client_challenge:08x} from client");
+
+    let mut response_cursor = Cursor::new(Vec::<u8>::new());
+    let mut response = BitWriter::endian(&mut response_cursor, LittleEndian);
+
+    response.write(32, CONNECTIONLESS_HEADER)?;
+    response.write(8, b'A')?; // S2C_CHALLENGE
+    response.write(32, 0x5a4f4933)?; // S2C_MAGICVERSION
+    response.write(32, server_challenge)?;
+    response.write(32, client_challenge)?;
+    response.write(32, PROTOCOL_HASHEDCDKEY)?;
+
+    packet.send(&response_cursor.into_inner())?;
+
+    Ok(())
+}
+
+pub fn process_connectionless_packet(packet: &PacketInfo) -> anyhow::Result<()> {
+    // Cut off connectionless header
+    let packet_data = packet
+        .data
+        .get(4..)
+        .ok_or_else(|| anyhow!("connectionless packet doesn't have any data"))?;
+
+    let mut reader = BitReader::endian(Cursor::new(packet_data), LittleEndian);
+
+    let command: u8 = reader.read(8)?;
+
+    match command {
+        // A2S_GETCHALLENGE
+        b'q' => handle_get_challenge(packet, &mut reader),
+        _ => Err(anyhow!(
+            "unhandled connectionless packet type: {} ({:?})",
+            command,
+            command as char
+        )),
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,65 @@
 mod connectionless;
 
-use std::{borrow::Cow, net::UdpSocket};
+use std::{
+    borrow::Cow,
+    net::{SocketAddr, UdpSocket},
+};
 
 use anyhow::anyhow;
 use log::{debug, info, trace};
 
-const CONNECTIONLESS_HEADER: [u8; 4] = (-1_i32).to_le_bytes();
-const SPLITPACKET_HEADER: [u8; 4] = (-2_i32).to_le_bytes();
-const COMPRESSEDPACKET_HEADER: [u8; 4] = (-3_i32).to_le_bytes();
+const CONNECTIONLESS_HEADER: u32 = -1_i32 as u32;
+const SPLITPACKET_HEADER: u32 = -2_i32 as u32;
+const COMPRESSEDPACKET_HEADER: u32 = -3_i32 as u32;
 
-fn decode_raw_packet(packet_buf: &[u8]) -> anyhow::Result<Cow<'_, [u8]>> {
-    let header_flags = packet_buf
+struct PacketInfo<'a> {
+    data: &'a [u8],
+    from: SocketAddr,
+    socket: &'a UdpSocket,
+}
+
+impl PacketInfo<'_> {
+    /// Send data to the address that this packet originated from
+    pub fn send(&self, data: &[u8]) -> std::io::Result<usize> {
+        self.socket.send_to(data, self.from)
+    }
+}
+
+fn decode_raw_packet(packet_data: &[u8]) -> anyhow::Result<Cow<'_, [u8]>> {
+    let header_flags = packet_data
         .get(0..4)
         .ok_or_else(|| anyhow!("couldn't get header flags"))?;
 
-    if header_flags == SPLITPACKET_HEADER {
+    if header_flags == SPLITPACKET_HEADER.to_le_bytes() {
         todo!("handle splitpacket");
     }
 
-    if header_flags == COMPRESSEDPACKET_HEADER {
+    if header_flags == COMPRESSEDPACKET_HEADER.to_le_bytes() {
         todo!("handle compressed packet");
     }
 
-    Ok(Cow::Borrowed(packet_buf))
+    Ok(Cow::Borrowed(packet_data))
 }
 
-fn process_packet(packet_buf: &[u8]) -> anyhow::Result<()> {
-    let packet_buf = decode_raw_packet(packet_buf)?;
+fn process_packet<'a>(
+    socket: &UdpSocket,
+    from: SocketAddr,
+    packet_data: &[u8],
+) -> anyhow::Result<()> {
+    let packet_data = decode_raw_packet(packet_data)?;
 
-    let header_flags = packet_buf
+    let header_flags = packet_data
         .get(0..4)
         .ok_or_else(|| anyhow!("couldn't get header flags"))?;
 
-    if header_flags == CONNECTIONLESS_HEADER {
-        connectionless::process_connectionless_packet(&packet_buf);
+    let packet_info = PacketInfo {
+        data: &packet_data,
+        from,
+        socket,
+    };
+
+    if header_flags == CONNECTIONLESS_HEADER.to_le_bytes() {
+        connectionless::process_connectionless_packet(&packet_info)?;
     } else {
         todo!("netchannels");
     }
@@ -48,14 +74,19 @@ fn main() -> anyhow::Result<()> {
     info!("bound to address {:?}", socket.local_addr()?);
 
     // This is *seriously* overkill, but we can fix it later;
-    let mut packet_buf = vec![0u8; u32::MAX.try_into().expect("cannot allocate packet buf")];
+    let mut packet_data = vec![
+        0u8;
+        u32::MAX
+            .try_into()
+            .expect("cannot allocate packet data buffer")
+    ];
     loop {
-        let (packet_size, addr) = socket.recv_from(&mut packet_buf)?;
-        let packet_buf = &packet_buf[..packet_size];
+        let (packet_size, addr) = socket.recv_from(&mut packet_data)?;
+        let packet_data = &packet_data[..packet_size];
 
         trace!("got packet of size {} from {:?}", packet_size, addr);
 
-        if let Err(err) = process_packet(packet_buf) {
+        if let Err(err) = process_packet(&socket, addr, packet_data) {
             debug!("malformed packet from {:?}: {:?}", addr, err);
         };
     }
